e1e173c2e90f3c36242f14527713a1b9924d600c
JoaquinWeston <JoaquinWeston>
2021-03-25 11:10:53 -0300

    Lab 3 con Pendietes.h

---
+++ REVERSI/src/pendientes.h
@@ -0,0 +1,20 @@
+/*
+ * pendientes.h
+ *
+ *  Created on: 25 mar. 2021
+ *      Author: lp1-2019
+ */
+
+#ifndef PENDIENTES_H_
+#define PENDIENTES_H_
+
+/*
+ * TODO verificar las funciones problematicas
+ * TODO agregar dos arreglos de caracteres que almacenen los nombres de los jugadores
+ * TODO tratar de conquistar el mundo
+ * TODO hacer una funcion que revise en un archivo de lectura/escritura y escriba el resultado de un juego terminado como estadisticas
+ * TODO agregar la interfaz grafica, ser lo mas informativo posible y arrazar con los puntos plus
+ * TODO robar la formula secreta de la cangreburguer
+ * */
+
+#endif /* PENDIENTES_H_ */
